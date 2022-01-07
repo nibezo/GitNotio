95a59897527f4c16bb4e9ff49205a81d4c08ecad
Ilya Yudin <nibezo.cs@gmail.com>
2022-01-07 11:13:59 +0300

    feat(pygithub): add comments with simple to-do, add file update example

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from github import Github
 import private_token
+# Open Notion page
+# Parse Notion page, if there are changes
+# Each Notion page = .md
+# Update git repo, create files, if there are new Notion pages (new .md)
 
-# using an access private_token.py
+# using for access private_token.py
 user = Github(private_token.token)
 # Create a list with your repositories
 repositories = [str(repo.full_name) for repo in user.get_user().get_repos()]
@@ -11,5 +15,9 @@ login = user.get_user().login
 
 # Create repo variable for
 repo = user.get_repo(f'{login}/gitnotiotest')
+contents = repo.get_contents('test.md', ref="test")
 
-# repo.create_file('test.md', '`hello!`', 'is it a commit?', branch='test')
+with open('Python/numpy.md', 'r') as file:
+    data = file.read()
+print(type(data))
+repo.update_file(contents.path, "it's new `file change` commit", data, contents.sha, branch="test")
